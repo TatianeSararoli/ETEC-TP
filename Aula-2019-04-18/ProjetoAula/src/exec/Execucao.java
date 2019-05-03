da620b675cf71c888280c74e363fa2bae9740676
tatianesararoli <tatianesararoli@gmail.com>
2019-05-02 22:21:47 -0300

    Aula-2019-04-18

---
+++ Aula-2019-04-18/ProjetoAula/src/exec/Execucao.java
@@ -0,0 +1,13 @@
+package exec;
+
+import aula.Pessoa;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoa ps = new Pessoa();
+		ps.andar();
+	}
+
+}
